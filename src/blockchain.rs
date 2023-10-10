6f2918415df886a93e5c7a2fa6f68c058d77d3ee
Younes <younesgt350@gmail.com>
2023-10-10 20:09:42 +0200

    changes

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -2,7 +2,7 @@ extern crate sha2;
 use sha2::{Sha256, Digest};
 use std::rand::Rng;
 use std::collections::HashSet;
-
+use std::time::{SystemTime, UNIX_EPOCH};
 
 
 
@@ -42,16 +42,24 @@ impl Block {
         
         let mut rng = rand::thread_rng();
         nonce = rng.gen_range(999..99999);  // nonce part of the block 
+
         if !nonceNum.contains(nonce) {
             let mut new_Nonce = rand::thread_rng().gen_range(999..99999);
         } else {
             !nonceNum.insert(nonce);
         }
 
-        
+        let start = SystemTime::now();
+        let since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Error while fetching the block timestamp");
+        let timestamp = since_the_epoch.as_secs() as i64;
+
+        prev_hash = match ZPHBlockchain.last() {
+            Some(value) => value.to_string(),
+            None => println!("The vector is empty"),
+        };
+         
         genhash = Sha256::new(); // hashing part of the block
-        let mut key = random_word(2);
-        genhash.update(key);
+        genhash.update(format!("{}{}{}", block_number, nonce, timestamp, prev_hash));
         hash = genhash.finalize();
 
 
